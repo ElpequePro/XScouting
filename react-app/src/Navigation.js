198b56407aceb4b878a90abee48a3d096b5f3c24
Elpeque Pro <elpeque9999@gmail.com>
2025-04-05 00:03:06 +0200

    create react app

---
+++ react-app/src/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navigation = () => {
+    return (
+        <nav>
+
+        </nav>
+    )
+}
+
+export default Navigation;
